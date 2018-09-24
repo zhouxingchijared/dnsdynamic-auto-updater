ee4941eea891b86c0b227f06d9e6af4242f24ab0
zhouxingchijay <zhouxingchijay@gmail.com>
2018-09-24 05:20:50 +0000

    Implemented a installation script that installs the systemd service and make the service load on boot

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+service_location="/etc/systemd/system/"
+service="test.service"
+pwd=$(pwd)
+
+cat > $service_location$service <<- _EOF
+[Unit]
+After=network.target
+
+[Service]
+ExecStart=$pwd/update-host.py
+
+[Install]
+WantedBy=default.target
+_EOF
+
+systemctl enable $service
+systemctl start $service
+echo "Installation complete"
